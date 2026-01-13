07e1dae7e0bc3f39e238875ad78c013ab9343e99
rosapalma <virginia.palma.ipm@upel.edu.ve>
2026-01-13 15:56:24 -0400

    FrontLogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,6 @@
 <x-guest-layout>
+
+    @section('title','SAITE -> Acceder')
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
@@ -24,24 +26,16 @@
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
+                <div align="center"> 
+                    <x-button>
+                        {{ __('ACCEDER') }}
+                    </x-button>
+                </div>
             </div>
         </form>
     </x-authentication-card>
 </x-guest-layout>
+
+
